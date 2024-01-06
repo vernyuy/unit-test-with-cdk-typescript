db6f994c7a43bca1e5c61058e849fcb427d83529
vernyuy <fonchu.e.venyuy@gmail.com>
2024-01-06 09:44:45 +0100

    Initial commit

---
+++ test/inventory_updates_system_typescript.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as InventoryUpdatesSystemTypescript from '../lib/inventory_updates_system_typescript-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/inventory_updates_system_typescript-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new InventoryUpdatesSystemTypescript.InventoryUpdatesSystemTypescriptStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
